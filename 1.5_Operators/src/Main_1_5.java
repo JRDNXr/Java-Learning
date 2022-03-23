3c172ba40848f5c4c067e68d379a28e66af20189
JRDNXr <jrdn_dsouza@hotmail.com>
2022-03-22 22:42:59 -0400

    Commit 1.5.1

---
+++ 1.5_Operators/src/Main_1_5.java
@@ -0,0 +1,60 @@
+
+// |===============|
+// |   OPERATORS   |
+// |===============|
+
+public class Main_1_5 {
+	
+	public static void main(String[] args) {
+		
+		/*
+			Arithmetic Operators:
+			
+				+	Addition		x + y
+				-	Subtraction		x - y
+				*	Multiplication	x * y
+				/	Division		x / y
+				%	Modulus			x % y
+				++	Increment		++x
+				--	Decrement		--x
+		
+		
+			Assignment Operators:
+			
+				=	Assign a value
+				+=
+				-=
+				*=
+				/=
+				%=
+				&=
+				|=
+				^=
+				>>=
+				<<=
+				
+				
+			Comparison Operators:
+			
+				==	Equal to
+				!=	Not equal to
+				>	Greater than
+				<	Less than
+				>=	Greater than or equal to
+				<=	Less than or equal to
+				
+				
+			Logical Operators:
+			
+				&&	Logical and
+				||	Logical or
+				!	Logical not
+		*/
+		
+		int num1 = 10;
+		
+		num1 += 5;
+		System.out.println(num1); // Output: 15
+	}
+
+}
