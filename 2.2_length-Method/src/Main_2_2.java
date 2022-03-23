ccfad2279a7597b99d39eef6955e2560e365e3a3
JRDNXr <jrdn_dsouza@hotmail.com>
2022-03-22 22:57:40 -0400

    Commit 2.0
    - Added all Java Tutorials to GitHub

---
+++ 2.2_length-Method/src/Main_2_2.java
@@ -0,0 +1,20 @@
+
+// |===================|
+// |   LENGTH METHOD   |
+// |===================|
+
+public class Main_2_2 {
+	
+	public static void main(String[] args) {
+		
+		// Length method counts the length of a string
+		
+		String line = "This is a line of text"; // 22 characters
+		
+		System.out.println(line.length());
+		
+		// The length method counts like humans do (by starting from 1)
+		
+	}
+
+}
