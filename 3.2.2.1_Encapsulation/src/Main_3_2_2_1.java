5de98335a9ed49b6678be2d5178edae968607fc2
JRDNXr <jrdn_dsouza@hotmail.com>
2022-04-10 12:53:22 -0400

    Commit 4.3
    - Made minor tweaks to 3.2.2.1_Encapsulation

--- 3.2.2.1_Encapsulation/src/Main_3_2_2_1.java
+++ 3.2.2.1_Encapsulation/src/Main_3_2_2_1.java
@@ -24,7 +24,7 @@ public class Main_3_2_2_1 {
 		// object1.name = "JRDN";
 		
 		// These lines of code will generate an error.
-		// This is because our class attribute "name" is set to private. This means that it can only be read within the declared class
+		// This is because our class attribute "name" is set to private. This means that it can only be read within the declared class.
 		
 		Main_3_2_2_1 object1 = new Main_3_2_2_1();
 		object1.setName("JRDN");
@@ -32,12 +32,11 @@ public class Main_3_2_2_1 {
 		System.out.println("First Name: " + object1.getName()); // Output: First Name: JRDN
 		
 		/* ** NOTES **
-		 * Encapsulation offers the following:
-		 * 		- Better control of class attributes and methods
-    	 *		- Class attributes can be made read-only (if you only use the get method), or write-only (if you only use the set method)
-    	 *		- Flexible: the programmer can change one part of the code without affecting other parts
-    	 *		- Increased security of data
-		 * 
+		 	Encapsulation offers the following:
+				- Better control of class attributes and methods
+    	 		- Class attributes can be made read-only (if you only use the get method), or write-only (if you only use the set method)
+    	 		- Flexible: the programmer can change one part of the code without affecting other parts
+    	 		- Increased security of data
 		 */
 	}
 }
